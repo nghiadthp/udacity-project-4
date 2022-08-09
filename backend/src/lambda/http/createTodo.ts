b7f35c9ad0da490922e09b9ce57351a3e5d1f1c6
Dung <dung.phammanh@petronas.com>
2022-08-10 00:24:25 +0700

    update BE

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,15 +3,35 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
+import { getToken } from '../../auth/utils'
+import { createLogger } from "../../utils/logger";
 import { createTodo } from '../../businessLogic/todos'
-
+const logger = createLogger("create-todo");
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
-
-    return undefined
+    logger.info('Start - Create todo function')
+    try {
+      const newTodo: CreateTodoRequest = JSON.parse(event.body)
+      const jwtToken = getToken(event.headers.Authorization)
+      const newCreatedTodo = await createTodo(newTodo, jwtToken)
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          item: newCreatedTodo
+        })
+      }
+    } catch (e) {
+      logger.error(e.message)
+      return {
+        statusCode: 500,
+        body: e.message
+      }
+    }
+  }
 )
 
 handler.use(
